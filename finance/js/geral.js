c7d6bfa6a73a5b344cf854c34cea2ee7ffbf60b6
Raul <raulerick@sempreceub.com>
2025-03-24 15:34:57 -0300

    v0.2 com mais funcionalidades na página de transação

--- finance/js/geral.js
+++ finance/js/geral.js
@@ -1,17 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Obtém o nome da página atual (por exemplo, 'dashboard.html')
-    const currentPage = window.location.pathname.split("/").pop();
 
-    // Seleciona todos os links do menu
-    const menuLinks = document.querySelectorAll('.menu li a');
-
-    // Itera sobre os links e adiciona a classe 'active' no link da página atual
-    menuLinks.forEach(link => {
-        if (link.href.includes(currentPage)) {
-            link.classList.add('active');
-        }
-    });
-});
 
 let userName = 'João'; 
 document.getElementById('user-name').textContent = userName;
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const addTransactionModal = new bootstrap.Modal(document.getElementById("addTransactionModal"));
+
+    document.querySelector('[data-bs-target="#addTransactionModal"]').addEventListener("click", function () {
+        addTransactionModal.show();
+    });
+});
